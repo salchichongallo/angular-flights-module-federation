827d38e76ddb6c878a1ef6c5257ba0f7c887f994
Jimmy Murillo <salchichongallo1997@gmail.com>
2023-05-24 09:56:12 -0500

    feat(shell): add header

---
+++ projects/shell/src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <header class="header">
+      <div class="header__content">
+        <img
+          src="assets/angular-logo.svg"
+          alt="Angular Flights Logo"
+          class="header__logo"
+        />
+        <nav class="menu">
+          <a
+            routerLink="/"
+            class="menu__item"
+            routerLinkActive="menu__item--active"
+            >Home</a
+          >
+        </nav>
+      </div>
+    </header>
+  `,
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {}
